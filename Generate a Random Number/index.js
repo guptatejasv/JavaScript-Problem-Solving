81d24085d9adb0b6a496f52cdbd16a841c7cba31
Tejasv Kumar <guptatejasv86086@gmail.com>
2024-06-24 17:39:07 +0530

    program8

--- Generate a Random Number/index.js
+++
@@ -1,8 +0,0 @@
-function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-let min = 1;
-let max = 100;
-let randomNumber = getRandomNumber(min, max);
-console.log(`Random number between ${min} and ${max}: ${randomNumber}`);
